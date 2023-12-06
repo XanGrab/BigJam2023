166349b7ce7f1219802bd27a42cfaa9453deb946
XanGrab <sunbird.grabowski@gmail.com>
2023-12-05 18:56:32 -0600

    Start from any scene

--- Assets/Scripts/Utils/SceneTransitioner.cs
+++ Assets/Scripts/Utils/SceneTransitioner.cs
@@ -2,37 +2,30 @@ using BeauRoutine;
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-public class SceneTransitioner : Singleton<SceneTransitioner>
-{
-    public enum SceneIndicies
-    {
-        Gameplay = 0
+public class SceneTransitioner : Singleton<SceneTransitioner> {
+    public enum SceneIndicies {
+        MainMenu,
+        Gameplay,
     }
 
-    private void Start()
-    {
-        LoadSceneWithIndex((int)SceneIndicies.Gameplay);
+    private void Start() {
+        // LoadSceneWithIndex((int)SceneIndicies.Gameplay);
     }
 
-    public void LoadSceneWithIndex(int _index)
-    {
+    public void LoadSceneWithIndex(int _index) {
         SceneManager.LoadScene(_index);
     }
 
-    public void WaitThenLoadScene(int _index, float _delay)
-    {
+    public void WaitThenLoadScene(int _index, float _delay) {
         Routine.Start(WaitThenLoadRoutine(_index, _delay));
     }
 
-    private IEnumerator WaitThenLoadRoutine(int _index, float _delay)
-    {
+    private IEnumerator WaitThenLoadRoutine(int _index, float _delay) {
         yield return _delay;
-
         SceneManager.LoadScene(_index);
     }
 
-    public void ReloadCurrScene()
-    {
+    public void ReloadCurrScene() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
