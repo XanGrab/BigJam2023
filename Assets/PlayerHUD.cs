fc6a3338c4693dcb62ec37988988decd60bf70f9
XanGrab <sunbird.grabowski@gmail.com>
2023-12-11 23:43:50 -0600

    Health Bar ❤️

---
+++ Assets/PlayerHUD.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+[RequireComponent(typeof(UIDocument))]
+public class PlayerHUD : MonoBehaviour {
+    [SerializeField] private UIDocument playerHUD;
+    [SerializeField] private PlayerStats playerStats;
+
+    private ProgressBar Healthbar;
+
+    // Start is called before the first frame update
+    public void Init() {
+        if(!playerHUD) return;
+
+        var root = playerHUD.rootVisualElement;
+        Healthbar = root.Q<ProgressBar>("HealthBar");
+
+        Debug.Log("DEBUG [PlayerHUD] getMaxHP: " + playerStats.getMaxHP());
+        Healthbar.highValue = playerStats.getMaxHP();
+        Healthbar.lowValue = 0;
+
+        Debug.Log("DEBUG [PlayerHUD] currHp: " + playerStats.getCurrHp());
+        Healthbar.value = playerStats.getCurrHp();
+    }
+
+    private void UpdateHealthBarValue(){
+        Healthbar.value = playerStats.getCurrHp();
+    }
+
+    private void OnEnable() {
+        playerStats.onHPChange += UpdateHealthBarValue;
+    }
+    private void OnDisable() {
+        playerStats.onHPChange -= UpdateHealthBarValue;
+    }
+}
