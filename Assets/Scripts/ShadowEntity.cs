ab0ecaca9346d6968d796595e6aaeed478c5dc38
TravisBird <47787636+Pyrousha@users.noreply.github.com>
2023-12-12 21:30:50 -0600

    Healthbar wheee

--- Assets/Scripts/ShadowEntity.cs
+++ Assets/Scripts/ShadowEntity.cs
@@ -5,24 +5,31 @@ public class ShadowEntity : MonoBehaviour {
 
     [SerializeField] private LayerMask groundLayer;
 
+    private Vector3 startScale;
+
     private void Start() {
         shadowTransform.gameObject.name = "shadow_" + name;
         shadowTransform.parent = null;
+
+        startScale = shadowTransform.localScale;
     }
 
     // Update is called once per frame
     void FixedUpdate() {
-        #region Set position and scale of "shadow" object
+        #region Set position and scale of "shadow" object   
         float newScale = 0;
         if (Physics.Raycast(transform.position + new Vector3(0, 1, 0), -transform.up, out RaycastHit _hit, 50, groundLayer))
         {
-            shadowTransform.position = _hit.point;
+            shadowTransform.position = _hit.point + new Vector3(0,-0.001f, 0);
             float distToGround = _hit.distance;
 
             //Shadow should be smaller the further away the character is from the ground
             newScale = Mathf.Max(1.5f - 0.1f * distToGround, 0);
         }
-        shadowTransform.localScale = new Vector3(newScale, shadowTransform.localScale.y, newScale);
+
+        Vector3 scaleToUse = startScale * newScale;
+        scaleToUse.y = startScale.y;
+        shadowTransform.localScale = scaleToUse;
         #endregion
     }
 
